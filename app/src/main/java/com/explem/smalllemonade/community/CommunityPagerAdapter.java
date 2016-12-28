18bad6f8870f8056cd02fa44d9131f245d5e9c1e
wb.bltx <wb.bltx@aliyun.com>
2016-12-28 22:16:26 +0800

    community wubinbin

--- app/src/main/java/com/explem/smalllemonade/community/CommunityPagerAdapter.java
+++ app/src/main/java/com/explem/smalllemonade/community/CommunityPagerAdapter.java
@@ -1,5 +1,8 @@
 package com.explem.smalllemonade.community;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 
@@ -7,14 +10,20 @@ import android.view.View;
  * Created by Administrator on 2016/12/28 0028.
  */
 //
-//public class CommunityPagerAdapter extends PagerAdapter {
-//    @Override
-//    public int getCount() {
-//        return 0;
+//public class CommunityPagerAdapter extends FragmentPagerAdapter {
+//
+//
+//    public CommunityPagerAdapter(FragmentManager fm) {
+//        super(fm);
 //    }
 //
 //    @Override
-//    public boolean isViewFromObject(View view, Object object) {
-//        return false;
+//    public Fragment getItem(int position) {
+//        return null;
+//    }
+//
+//    @Override
+//    public int getCount() {
+//        return 0;
 //    }
 //}
