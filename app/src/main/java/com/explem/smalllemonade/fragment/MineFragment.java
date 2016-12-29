a4b7f15ec26d1b04b99a8fe0ead78f0cb179d180
Administrator <1184525630@qq.com>
2016-12-29 09:34:37 +0800

    by 薛亚南 2016/12/29

--- app/src/main/java/com/explem/smalllemonade/fragment/MineFragment.java
+++ app/src/main/java/com/explem/smalllemonade/fragment/MineFragment.java
@@ -1,16 +1,33 @@
 package com.explem.smalllemonade.fragment;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.explem.smalllemonade.R;
 import com.explem.smalllemonade.base.BaseFragment;
+import com.explem.smalllemonade.utils.CommonUtils;
 import com.explem.smalllemonade.view.ShowingPage;
+
+import static com.explem.smalllemonade.R.id.mf_icon;
+import static com.explem.smalllemonade.R.id.mf_ivSex;
+import static com.explem.smalllemonade.R.id.mf_reFeed;
+import static com.explem.smalllemonade.R.id.mf_reMine;
+import static com.explem.smalllemonade.R.id.mf_reSeting;
+import static com.explem.smalllemonade.R.id.mf_tvNum;
 
 /**
  * Created by Pooh on 2016/12/27.
  */
 
-public class MineFragment extends BaseFragment{
+public class MineFragment extends BaseFragment {
+
+    private View mineView;
+    private ImageView mf_icon,mf_ivSex;
+    private TextView mf_tvName,mf_tvNum;
+    private RelativeLayout mf_reBaseInfo,mf_reMine,mf_reFeed,mf_reSeting;
+
     @Override
     protected void onload() {
         //设置布局
@@ -19,8 +36,24 @@ public class MineFragment extends BaseFragment{
 
     @Override
     protected View createSuccessView() {
-        TextView tv=new TextView(getActivity());
-        tv.setText("我的.......");
-        return tv;
+        mineView = CommonUtils.inflate(R.layout.fragment_mine);
+        initView();
+        return mineView;
+    }
+
+    private void initView() {
+        //头像
+        mf_icon = (ImageView) mineView.findViewById(R.id.mf_icon);
+        //昵称
+        mf_tvName = (TextView) mineView.findViewById(R.id.mf_tvName);
+        //性别
+        mf_ivSex = (ImageView) mineView.findViewById(R.id.mf_ivSex);
+        //关注数量
+        mf_tvNum = (TextView) mineView.findViewById(R.id.mf_tvNum);
+         //基本资料
+        mf_reBaseInfo = (RelativeLayout) mineView.findViewById(R.id.mf_reBaseInfo);
+        mf_reMine = (RelativeLayout) mineView.findViewById(R.id.mf_reMine);
+        mf_reFeed = (RelativeLayout) mineView.findViewById(R.id.mf_reFeed);
+        mf_reSeting = (RelativeLayout) mineView.findViewById(R.id.mf_reSeting);
     }
 }
