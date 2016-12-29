6dca3ed1084c431cbed76787beb698c151fd74ce
Administrator <1184525630@qq.com>
2016-12-29 11:21:19 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/dimens.xml

--- app/src/main/java/com/explem/smalllemonade/fragment/MineFragment.java
+++ app/src/main/java/com/explem/smalllemonade/fragment/MineFragment.java
@@ -12,6 +12,7 @@ import com.explem.smalllemonade.view.ShowingPage;
 
 import static com.explem.smalllemonade.R.id.mf_icon;
 import static com.explem.smalllemonade.R.id.mf_ivSex;
+import static com.explem.smalllemonade.R.id.mf_reBaseInfo;
 import static com.explem.smalllemonade.R.id.mf_reFeed;
 import static com.explem.smalllemonade.R.id.mf_reMine;
 import static com.explem.smalllemonade.R.id.mf_reSeting;
@@ -21,12 +22,12 @@ import static com.explem.smalllemonade.R.id.mf_tvNum;
  * Created by Pooh on 2016/12/27.
  */
 
-public class MineFragment extends BaseFragment {
+public class MineFragment extends BaseFragment implements View.OnClickListener {
 
     private View mineView;
-    private ImageView mf_icon,mf_ivSex;
-    private TextView mf_tvName,mf_tvNum;
-    private RelativeLayout mf_reBaseInfo,mf_reMine,mf_reFeed,mf_reSeting;
+    private ImageView mf_icon, mf_ivSex;
+    private TextView mf_tvName, mf_tvNum;
+
 
     @Override
     protected void onload() {
@@ -50,10 +51,29 @@ public class MineFragment extends BaseFragment {
         mf_ivSex = (ImageView) mineView.findViewById(R.id.mf_ivSex);
         //关注数量
         mf_tvNum = (TextView) mineView.findViewById(R.id.mf_tvNum);
-         //基本资料
-        mf_reBaseInfo = (RelativeLayout) mineView.findViewById(R.id.mf_reBaseInfo);
-        mf_reMine = (RelativeLayout) mineView.findViewById(R.id.mf_reMine);
-        mf_reFeed = (RelativeLayout) mineView.findViewById(R.id.mf_reFeed);
-        mf_reSeting = (RelativeLayout) mineView.findViewById(R.id.mf_reSeting);
+
+        mineView.findViewById(mf_reBaseInfo).setOnClickListener(this);
+        mineView.findViewById(R.id.mf_reMine).setOnClickListener(this);
+        mineView.findViewById(R.id.mf_reFeed).setOnClickListener(this);
+        mineView.findViewById(R.id.mf_reSeting).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            //基本资料
+            case R.id.mf_reBaseInfo:
+                break;
+            //我的帖子
+            case R.id.mf_reMine:
+                break;
+            //意见反馈
+            case R.id.mf_reFeed:
+                break;
+            //设置
+            case R.id.mf_reSeting:
+                break;
+        }
+
     }
 }
